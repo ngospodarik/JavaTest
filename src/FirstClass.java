ebae6b790f4ef81dce3114b3beda25aef957ce34
Nikolay Gospodarik <n@gospodarik.ru>
2024-02-03 15:55:37 +0300

    test commit

---
+++ src/FirstClass.java
@@ -0,0 +1,27 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Нет войне!");
+        System.out.println();
+
+		/*
+		 defining variables
+		 */
+
+        int x = 5;
+
+        double y = 5.0;
+
+        boolean flag = true;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("flag:" + flag);
+
+
+
+
+    }
+
+}
